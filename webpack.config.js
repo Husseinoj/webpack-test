fe88e94538ca763d32b3449bfc1c2c935c908a65
Hussein Ojaghi <h.ojaghi@roundtableapps.com>
2020-04-12 15:26:57 +0430

    add babel loader to transpile es-js to older js version and also html-webpack-plugin to generate html in dist

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports =  {
+    entry: './src/app.js',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module:{
+        rules:[
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins:[
+        new htmlWebpackPlugin({
+            title:"Output Message"
+        })
+    ]
+}
